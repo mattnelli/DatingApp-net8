0b2637101eee493e1ba91a4a8d2094de53e9cb1f
mattnelli <64448691+mattnelli@users.noreply.github.com>
2025-03-31 18:02:06 -0400

    tree

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,24 +3,27 @@ import {FormsModule } from '@angular/forms';
 import { AccountService } from '../_service/account.service';
 import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, NgIf, BsDropdownModule],
+  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink,RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService); 
-
+  private router = inject(Router)
+  private toastr = inject(ToastrService)
   model: any = {};
 
   login(){
    this.accountService.login(this.model).subscribe({
-      next: Response => {
-        console.log(Response);
+      next: _ => {
+        this.router.navigateByUrl("/members")
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
 
 
    });
@@ -29,6 +32,7 @@ export class NavComponent {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl("")
   }
 
 
